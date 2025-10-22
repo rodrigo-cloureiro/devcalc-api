4c4d1000fd772aa126b98a6b17acac305a23537a
Rodrigo Loureiro <rodrigo@MacBook-Air-de-Rodrigo.local>
2025-10-22 19:10:16 -0300

    iniciando projeto (spring initializr)

---
+++ src/main/java/br/com/infnet/devcalc/DevcalcApplication.java
@@ -0,0 +1,13 @@
+package br.com.infnet.devcalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevcalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevcalcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/infnet/devcalc/DevcalcApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.infnet.devcalc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevcalcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
