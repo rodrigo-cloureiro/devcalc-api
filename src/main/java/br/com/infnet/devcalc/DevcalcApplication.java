cf31560520cd946af67210d9d8b7a8febe74a3c8
rodrigo-cloureiro <rodrigo.cloureiro@al.infnet.edu.br>
2025-10-22 22:16:11 -0300

    eliminando linhas em branco no main

--- src/main/java/br/com/infnet/devcalc/DevcalcApplication.java
+++ src/main/java/br/com/infnet/devcalc/DevcalcApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DevcalcApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(DevcalcApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(DevcalcApplication.class, args);
+    }
 }
